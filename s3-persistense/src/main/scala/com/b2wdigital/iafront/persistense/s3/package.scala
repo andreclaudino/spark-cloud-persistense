5a24b1cb1fe9321aa47c9c6643bd366628219242
André Claudino <andre.claudino@b2wdigital.com>
2019-12-04 11:22:05 -0300

    * Remove autenticação por variáveis de ambiente para S3A * Adiciona parâmetros para otimizar integração com S3

--- s3-persistense/src/main/scala/com/b2wdigital/iafront/persistense/s3/package.scala
+++ s3-persistense/src/main/scala/com/b2wdigital/iafront/persistense/s3/package.scala
@@ -11,12 +11,13 @@ package object s3 {
       setHadoopConf("fs.s3a.access.key", "AWS_ACCESS_KEY_ID")(sparkSession)
       setHadoopConf("fs.s3a.secret.key", "AWS_SECRET_ACCESS_KEY")(sparkSession)
 
-      setHadoopConf("fs.s3a.awsAccessKeyId", "AWS_ACCESS_KEY_ID")(sparkSession)
-      setHadoopConf("fs.s3a.sawsSecretAccessKey", "AWS_SECRET_ACCESS_KEY")(sparkSession)
+      sparkSession.conf.set("spark.hadoop.fs.s3a.multiobjectdelete.enable","false")
+      sparkSession.conf.set("spark.hadoop.fs.s3a.fast.upload","true")
+      sparkSession.conf.set("spark.sql.parquet.filterPushdown", "true")
+      sparkSession.conf.set("spark.sql.parquet.mergeSchema", "false")
+      sparkSession.conf.set("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
+      sparkSession.conf.set("spark.speculation", "false")
 
-
-      sparkSession.conf.set("spark.hadoop.fs.s3a.aws.credentials.provider",
-        "com.amazonaws.auth.EnvironmentVariableCredentialsProvider")
       sparkSession.conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     }
   }
